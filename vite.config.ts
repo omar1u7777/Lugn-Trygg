ce3fdd65b40b224b5426c6e73bfe075060eb3e8b
omar1u7777 <omaralhaek97@gmail.com>
2025-10-28 15:51:38 +0100

    Fix: Enable automatic JSX runtime in Vite to prevent 'React is not defined' errors, remove conflicting CSP meta tag

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic', // Use new JSX transform to avoid needing React import for JSX
+  })],
   build: {
     outDir: 'dist',
     sourcemap: false,
