5f654fcaf3a49f37fd0657537065cb9228d993f7
Omar Ahmad Alhaek 0013 <omar_ahmad.alhaek0013@stud.hkr.se>
2026-01-02 00:40:49 +0100

    Cleanup: Remove 23 unused frontend components and 3 empty directories

--- src/components/Dashboard/Layout/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DashboardLayout } from './DashboardLayout';
-export { default as DashboardHeader } from './DashboardHeader';
-export { default as DashboardGrid } from './DashboardGrid';
-export { default as DashboardSection } from './DashboardSection';
--- src/components/Dashboard/Widgets/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as BaseWidget } from './BaseWidget';
-export { default as StatCard } from './StatCard';
-export { default as ActionCard } from './ActionCard';
